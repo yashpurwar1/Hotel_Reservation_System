bed11d02599b56ae9a5163472a6f79b8c8c2550d
yashpurwar1 <yashpurwar.yashpurwar.com>
2021-08-04 08:36:23 +0530

    Added: Welcome message

---
+++ src/HotelManagement/HotelReservation.java
@@ -0,0 +1,7 @@
+package HotelManagement;
+
+public class HotelReservation {
+    public static void main (String[] args){
+        System.out.println("Welcome to hotel reservation system");
+    }
+}
